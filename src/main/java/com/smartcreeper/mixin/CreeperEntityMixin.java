85c2ae434ff25a692cd96d9ef8bd84df23d430e6
MR OK <hojayfai3@gmail.com>
2025-06-23 19:51:00 +0600

    CreeperEntityMixin.java

--- src/main/java/com/smartcreeper/mixin/CreeperEntityMixin.java
+++ src/main/java/com/smartcreeper/mixin/CreeperEntityMixin.java
@@ -1,6 +1,6 @@
 package com.smartcreeper.mixin;
 ‎
-‎import com.smartcreeper.features.DoorOpen;
+‎import com.smartcreeper.features.*;
 ‎import net.minecraft.entity.mob.CreeperEntity;
 ‎import org.spongepowered.asm.mixin.Mixin;
 ‎import org.spongepowered.asm.mixin.injection.At;
